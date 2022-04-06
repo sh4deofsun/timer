daa0d91c08702b12e02f83c73700aa73bd8b9379
Batuhan <by.yalcin.batuhan@gmail.com>
2022-04-06 16:40:25 +0300

    takeing trash out and adding ads

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from __future__ import barry_as_FLUFL
 import time
 import tkinter as tk
 import random
-
-from streamlit import stop
 
 rootWindow = tk.Tk()
 rootWindow.title("Timer")
@@ -47,7 +44,7 @@ def rndAds():
     "Jenkins: This is how we do automation",
     "This is the way!",
     "Oh! You finally awake",
-    "Batuhan not recommend to use RF v3.2.2",
+    "Batuhan not recommend to use RF v3",
     "Did you know? TDM built with Django",
     "Did you know what are these? BDD, KDD, DDD",
     "We have best Scrum master and her name is Esra",
@@ -55,7 +52,9 @@ def rndAds():
     "Hold my beer, I got this bug",
     "Coffee! You can sleep when you're dead!",
     "Let's talk about when we gonna do Happy Hour",
-    "Happy hours is blesed times"]
+    "Happy hours is blesed times",
+    "A QA Tester has no name",
+    "If they fix their bug, Initiate Order 66"]
     return random.choice(adList)
 
 def refreshAd():
@@ -66,11 +65,8 @@ def refreshAd():
 frm_clock = tk.Frame(master=rootWindow)
 ent_clockMin = tk.Entry(master=frm_clock,width=6,textvariable=timerMin,font=('Times New Roman', 40))
 ent_clockMin.grid(row=0,column=0)
-# timerMin = ent_clockMin.get()
 ent_clockSec = tk.Entry(master=frm_clock,width=6,textvariable=timerSec,font=('Times New Roman', 40))
 ent_clockSec.grid(row=0,column=1)
-# timerSec = ent_clockSec.get()
-# frm_clock.grid(row=0,column=0)
 frm_clock.grid(row=0,column=0)
 
 # Start - Stop buttons
